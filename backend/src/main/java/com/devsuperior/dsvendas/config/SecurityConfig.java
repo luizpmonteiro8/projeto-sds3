c05cf02495ada8a8f363e3193139267c06ad4dfa
Luiz <luizpmonteiro8@hotmail.com>
2021-09-10 17:05:09 -0300

    Routes

--- backend/src/main/java/com/devsuperior/dsvendas/config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dsvendas/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	  @Override protected void configure(HttpSecurity http) throws Exception {
 	  
 	  if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
-		  http.headers().frameOptions().disable();
+		 http.headers().frameOptions().disable();
 	   }
 	  
 	  http.cors().and().csrf().disable();
